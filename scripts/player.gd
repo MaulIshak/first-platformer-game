0e3eeb9ed2ebc0ac0f9b1bb610e28a8113d5c382
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-03-01 15:55:27 +0700

    add label including score, add game manager, add bgm, add coin sfx

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 
 
 func _physics_process(delta):
@@ -17,9 +19,26 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# direction : -1 , 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	# flip sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+
+	
+	# move player
 	if direction:
 		velocity.x = direction * SPEED
 	else:
